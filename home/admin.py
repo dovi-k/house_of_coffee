c4fbc875ac492450fd6a7d4d333bb71572f013df
dovi-k <dovylee@gmail.com>
2021-10-28 09:42:08 +0000

    Fixed bug: Check out KeyError with country field adjusted in checkout app models.py file

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import HomePage
 
 # Register your models here.
+
+class HomeAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'button_text',
+    )
+
+
+# Register your models here.
+admin.site.register(HomePage, HomeAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class HomePage(models.Model):
+    title = models.CharField(max_length=64, null=False)
+    button_text = models.CharField(max_length=32, null=False)
+
+    def __str__(self):
+        return self.button_text
