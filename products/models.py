ac30c0c564aac0674271485e10b649c39d2d2f3e
dovi-k <dovylee@gmail.com>
2021-10-30 22:29:17 +0000

    Added Footer to base.html

--- products/models.py
+++ products/models.py
@@ -16,12 +16,10 @@ class Category(models.Model):
         return self.friendly_name
 
 
-class Syrups(models.Model):
+class Syrup(models.Model):
       
     name = models.CharField(max_length=50)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    unit_of_measure = models.CharField(max_length=25, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -33,8 +31,7 @@ class Syrups(models.Model):
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True,
                                  on_delete=models.SET_NULL)
-    syrups = models.ForeignKey('Syrups', null=True, blank=True,
-                                 on_delete=models.SET_NULL)
+    syrup = models.ForeignKey('Syrup', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     type = models.CharField(max_length=254, blank=True)
@@ -43,7 +40,6 @@ class Product(models.Model):
     group = models.CharField(max_length=50, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     unit_of_measure = models.CharField(max_length=25, null=True, blank=True)
-    #image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
