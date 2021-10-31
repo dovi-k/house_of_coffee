852158be5c61b8ce96cd995330faa8153591bcb3
dovi-k <dovylee@gmail.com>
2021-10-31 19:36:40 +0000

    Deployment: Added allowed hosts and Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn house_of_coffee.wsgi:application
--- house_of_coffee/settings.py
+++ house_of_coffee/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-gxotny!tv(iwa)-)153)xcdggjf%=*098r87d9n(kt!r)@r@)g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['house-of-coffee.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -125,12 +125,17 @@ WSGI_APPLICATION = 'house_of_coffee.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    } 
 
 
 # Password validation
